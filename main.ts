47ca0578e778755f227cb01c962b071fa869af1d
VivaSitani <66580922+VivaSitani@users.noreply.github.com>
2021-08-16 10:59:56 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -8,8 +8,8 @@ input.onButtonPressed(Button.B, function () {
     car += 1
     led.plot(car, 4)
 })
-let obstacle1 = 0
 let obstacle2 = 0
+let obstacle1 = 0
 let car = 0
 let lives = 5
 let score = 0
@@ -17,13 +17,42 @@ car = 2
 basic.clearScreen()
 led.plot(car, 4)
 basic.forever(function () {
-    basic.pause(200)
     if (lives <= 0) {
         basic.clearScreen()
         basic.pause(500)
-        basic.showString("GAME OVER")
-        basic.showString("SCORE")
-        basic.showString("" + (game.score()))
+        game.gameOver()
+        game.setScore(score)
+    } else {
+        obstacle1 = randint(0, 4)
+        led.plot(obstacle1, 0)
+        basic.pause(100)
+        led.unplot(obstacle1, 0)
+        led.plot(obstacle1, 1)
+        basic.pause(100)
+        led.unplot(obstacle1, 1)
+        led.plot(obstacle1, 2)
+        basic.pause(100)
+        led.unplot(obstacle1, 2)
+        led.plot(obstacle1, 3)
+        basic.pause(100)
+        led.unplot(obstacle1, 3)
+        led.plot(obstacle1, 4)
+        basic.pause(100)
+        led.unplot(obstacle1, 4)
+        if (obstacle1 == car) {
+            lives += -1
+            led.plot(car, 4)
+        } else {
+            score += 1
+            game.setScore(score)
+        }
+        basic.pause(100)
+    }
+})
+basic.forever(function () {
+    basic.pause(200)
+    if (lives <= 0) {
+        basic.pause(100)
     } else {
         obstacle2 = randint(0, 4)
         led.plot(obstacle2, 0)
@@ -47,39 +76,6 @@ basic.forever(function () {
         } else {
             score += 1
         }
-    }
-})
-basic.forever(function () {
-    if (lives <= 0) {
-        basic.clearScreen()
-        basic.pause(500)
-        basic.showString("GAME OVER")
-        basic.showString("SCORE")
-        basic.showString("" + (game.score()))
-    } else {
-        obstacle1 = randint(0, 4)
-        led.plot(obstacle1, 0)
-        basic.pause(100)
-        led.unplot(obstacle1, 0)
-        led.plot(obstacle1, 1)
-        basic.pause(100)
-        led.unplot(obstacle1, 1)
-        led.plot(obstacle1, 2)
-        basic.pause(100)
-        led.unplot(obstacle1, 2)
-        led.plot(obstacle1, 3)
-        basic.pause(100)
-        led.unplot(obstacle1, 3)
-        led.plot(obstacle1, 4)
-        basic.pause(100)
-        led.unplot(obstacle1, 4)
-        if (obstacle1 == car) {
-            lives += -1
-            led.plot(car, 4)
-        } else {
-            score += 1
-        }
-        basic.pause(100)
     }
 })
 basic.forever(function () {
